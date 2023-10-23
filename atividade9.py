c01ae4a2bb2d50589c2096bfe724ecc2d2e16767
33669933 <vivianmariasantos825@gmail.com>
2023-10-23 10:21:11 -0300

    first commit

---
+++ atividade9.py
@@ -0,0 +1,8 @@
+n1 = float(input("digite o primeiro numero:"))
+n2 = float(input("digite o segundo numero:"))
+if n1 < n2:
+   print(f"O {n1} é menor que o {n2} ")
+elif n1 > n2:
+   print(f"O {n1} é maior que o {n2}")
+else: 
+   print("os numeros são iguais.")
